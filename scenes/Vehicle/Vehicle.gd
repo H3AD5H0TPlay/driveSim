6095ffc3fbdcdf8b332aa754777bb0d6218c78cd
H3AD5H0TPlay <vpeter993@gmail.com>
2026-06-24 19:49:12 +0200

    Add WASD support and improve camera angle

--- scenes/Vehicle/Vehicle.gd
+++ scenes/Vehicle/Vehicle.gd
@@ -7,17 +7,17 @@ const MAX_STEER_ANGLE = 0.5
 func _physics_process(delta):
 	# Alapvető manuális irányítás a teszteléshez
 	var steer_val = 0.0
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("ui_left") or Input.is_physical_key_pressed(KEY_A):
 		steer_val = MAX_STEER_ANGLE
-	elif Input.is_action_pressed("ui_right"):
+	elif Input.is_action_pressed("ui_right") or Input.is_physical_key_pressed(KEY_D):
 		steer_val = -MAX_STEER_ANGLE
 	
 	steering = lerp(steering, steer_val, 5.0 * delta)
 
 	var accel = 0.0
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("ui_up") or Input.is_physical_key_pressed(KEY_W):
 		accel = MAX_ENGINE_FORCE
-	elif Input.is_action_pressed("ui_down"):
+	elif Input.is_action_pressed("ui_down") or Input.is_physical_key_pressed(KEY_S):
 		accel = -MAX_ENGINE_FORCE
 
 	engine_force = accel
